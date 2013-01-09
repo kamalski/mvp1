293ef01c4091e71df8bfba660cd839fb897ec73c
Mohamed <kamal@gigturn.com>
2013-01-09 10:54:34 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mvp1::Application.config.secret_token = '17a7f1f19d9f464fab2e7f72328be68a918bf8e102f8e2f14b4ed767158d96909f4b5cb77fe9adbeb207a6755010a0cf4fa2e2123bd54ef3720de350bab9d387'
